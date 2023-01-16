d34ee1ecbf149f0ca48050da380109561eabe095
nilokcu <nilarolat@gmail.com>
2023-01-16 13:36:54 +0100

    PS commit

---
+++ FolderMap-NTFS.ps1
@@ -0,0 +1,5 @@
+﻿$path = "D:\Share\Allgemein" #define path to the shared folder
+Tree $path /A
+$reportpath ="C:\Users\nokcu\Desktop\tree\allgemein.csv" #define path to export permissions report
+#script scans for directories under shared folder and gets acl(permissions) for all of them
+dir -Recurse $path | where { $_.PsIsContainer -eq "TRUE" } | % { $path1 = $_.fullname; Get-Acl $_.Fullname | % { $_.access | Add-Member -MemberType NoteProperty '.\Application Data' -Value $path1 -passthru }} | Export-Csv $reportpath
